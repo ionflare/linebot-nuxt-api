40dfa3146f0a24112613e179702a0e678505c1f2
ionflare <ccionflare@gmail.com>
2018-09-11 08:01:55 +0000

    test reply userid

--- api/index.js
+++ api/index.js
@@ -54,10 +54,10 @@ function handleEvent(event) {
   }
 
   // create a echoing text message
-  //const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: event.message.text };
 
   // use reply API
-  return client.replyMessage(event.replyToken, event.userId);
+  return client.replyMessage(event.replyToken, echo);
   /*
      var _mailbox = new MailBox({
                 
