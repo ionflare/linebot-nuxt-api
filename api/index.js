c56b0354c95b59b99603c2eed9df0eb522c1ecb0
ionflare <ccionflare@gmail.com>
2018-09-11 10:48:27 +0000

    fix mongoose

--- api/index.js
+++ api/index.js
@@ -2,9 +2,8 @@
 require('./config/config.js') //config for mongodb, jws
 
 const ObjectId = require('mongoose').Types.ObjectId; 
-
+const { mongoose } = require('./db/mongoose');
 const { MailBox } = require("./models/mailbox")
-
 const Client = require('@line/bot-sdk').Client;
 
 
@@ -25,27 +24,85 @@ const client = new line.Client(config);
 const app = express();
 
 
-app.get('/webhook',async(req,res)=>{
+app.get('/webhook', async(req,res)=>{
     
    //req.session.current_user = null;
    res.send("xxx" );
  
 })
 
-app.get('/testdb',async(req,res)=>{
+app.get('/testdb', async (req,res)=>{
     
    //req.session.current_user = null;
+   /*
+     User.find({
+        //all
+    }).then((userlists)=>{ res.send({ userlists } );
+    }).catch((e)=> { res.status(400).send(e) } );
+   */
+   
+   
+   
+   
     var _mailbox = new MailBox({
                 
                  from_user_id :  "xxx",
                  to_user_id :  "yyy",
                  message : "zzz",
-                 lastupdate : new Date().getTime(),
+                 lastupdate : new Date().getTime()
             });
-   await _mailbox.save();
+    //_mailbox.save();
    
-   
-   await res.send("xxx" );
+    await _mailbox.save();
+    
+    await res.redirect('/');
+    
+    //res.send("xxx" );
+    /*
+      var _user_role_1 = new User_Role({
+                roleName : "Admin",
+                roleId : 3,
+                accessibilityLV: 3,
+                isActive : true,
+                lastupdate : new Date().getTime()
+            });
+           
+            var _user_role_2 = new User_Role({
+               
+              
+                roleName : "Provider",
+                roleId : 2,
+                accessibilityLV: 2,
+                isActive : true,
+                lastupdate : new Date().getTime()
+            });
+          
+          var _user_role_3 = new User_Role({
+               
+              
+                roleName : "Secretary",
+                accessibilityLV: 1,
+                roleId : 1,
+                isActive : true,
+                lastupdate : new Date().getTime()
+            });
+         
+            var _user_role_4 = new User_Role({
+               
+              
+                roleName : "Customer",
+                accessibilityLV: 0,
+                roleId : 0,
+                isActive : true,
+                lastupdate : new Date().getTime()
+            });
+           await _user_role_1.save();
+           await _user_role_2.save();
+           await _user_role_3.save();
+           await _user_role_4.save();
+           await res.redirect('/');
+           */
+    
  
 })
 
